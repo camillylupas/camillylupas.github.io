88113f1b3e56ef9d2786dd1788e05925526b32b4
camillylupas <50755116+camillylupas@users.noreply.github.com>
2019-09-17 21:48:19 -0300

    freeway

---
+++ Sequoia_fox_copy_2019_09_18_00_43_58/sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+createCanvas(500, 400);
+}
+
+function draw() {
+background(imagemDaEstrada); 
+mostraAtor();
+movimentaAtor();
+mostraCarro();
+movimentaCarro();
+verificaColisao();
+  mostraPlacar();
+ 
+    
+}//Fim do Draw
+
+
+
+
+
